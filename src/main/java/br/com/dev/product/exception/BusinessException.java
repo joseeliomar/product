dd3dd7bf7fed9bd1f2f123004eb56de73cfb2f88
José Eliomar <jose.eliomar2244@gmail.com>
2025-09-21 09:40:31 -0300

    Fixed exception type.

--- src/main/java/br/com/dev/product/exception/BusinessException.java
+++ src/main/java/br/com/dev/product/exception/BusinessException.java
@@ -2,14 +2,14 @@ package br.com.dev.product.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class InternalServerErrorException extends ExceptionWithHttpStatus {
+public class BusinessException extends ExceptionWithHttpStatus {
     private static final long serialVersionUID = 1L;
 
-    public InternalServerErrorException(String message) {
+    public BusinessException(String message) {
         super(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    public InternalServerErrorException(String message, Throwable cause) {
+    public BusinessException(String message, Throwable cause) {
         super(message, cause, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
